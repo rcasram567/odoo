4eca980d41ff9dc00d582aa52cd4a21cb8929415
raulitorbk23 <raulitorbk23@gmail.com>
2024-11-04 10:15:53 +0100

    practica 2 - creando nuestro nav-menu

---
+++ ModulosNoNativos/Inventarios/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Modulo Inventarios',
+    'version': '1.0',
+    'depends': ['base'],
+    'author': 'Manolo Hernandez',
+    'description': 'Módulo donde indicamos el inventario de Odoo',
+    'data': ['views/navmenu.xml'],
+    'installable': True,
+}
---
+++ ModulosNoNativos/Inventarios/models/presupuesto.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+class Presupuesto(models.Model):
+    _name="presupuesto"
+    name=fields.Char(string='Nombre',required=True)
+    description = fields.Text(string="Description")
+    price = fields.Float(string='Precio', digits=(10,2))
+    status = fields.Selection([
+        ('draft', 'Borrador'),
+        ('confirmed', 'Confirmado'),
+        ('done', 'Hecho')
+    ], string='Estado', default='draft')
+    is_active = fields.Boolean(string='Activo', default=True)
+    start_date = fields.Date(string='Fecha de Inicio')
